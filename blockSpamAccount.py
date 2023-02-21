db0943625c9efbb546c7265588dcedcce3d3ea8f
juhojang <juhojang@hotmail.com>
2023-02-22 02:00:06 +0900

    해시태그 탐색 속도 향상

--- blockSpamAccount.py
+++ blockSpamAccount.py
@@ -6,7 +6,7 @@ from selenium.common.exceptions import UnexpectedAlertPresentException
 import time
 
 
-driver= webdriver.Edge(r"C:/msedgedriver.exe")
+driver= webdriver.Chrome(r"C:/chromedriver.exe")
 driver.implicitly_wait(3)
 driver.get('https://www.instagram.com/login')
 login_x_path='/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]/button'
@@ -17,7 +17,6 @@ setting_x_path='/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/d
 block_button_x_path='/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div/button[1]'
 real_block_x_path='/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/button[1]'
 cancel_x_path='/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/button'
-search_result_x_path='/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div/a/div/div[2]/div[1]/div/div'
 
 
 print("id를 입력하세요.")
@@ -29,10 +28,12 @@ try :
     driver.find_element('name','username').send_keys(id)
     driver.find_element('name','password').send_keys(password)
     driver.find_element('xpath',login_x_path).click()
+    time.sleep(1)
 except UnexpectedAlertPresentException:
     print("hi")
 try:
     driver.find_element('xpath',login_x_path).click()
+    time.sleep(1)
 except :
     print("bye")
 
@@ -41,38 +42,44 @@ list=['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
 
 for j in list:
     i=1
+    driver.refresh()
     while 1:
-        time.sleep(1)
-    
         try:
             driver.find_element('xpath',search_x_path).click()
+            driver.find_element('xpath',search_text_x_path).clear()
             driver.find_element('xpath',search_text_x_path).send_keys("부업"+j)
+            time.sleep(1)
+            isHashTag=driver.find_element('xpath','/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div['+str(i)+']/div/a/div/div[2]/div[1]/div/div').text
+            print(i)
+            try:
+                isLocation=driver.find_element('xpath','/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div['+str(i)+']/div/a/div/div[1]/div/svg').get_attribute("aria-label")
+                print(isLocation)
+            except:
+                print("cant find location")
+                pass
+            if isHashTag[0]=='#' or isLocation=='위치':
+                i=i+1
+                driver.find_element('xpath',search_x_path).click()
+                continue
         except:
-            pass
+                print("there is no list")
+                break
     
         try:
-            print('/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div['+str(i)+']/div/a/div')
             driver.find_element('xpath','/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div['+str(i)+']/div/a/div').click()
-            time.sleep(1)
         except:
             try:
-                driver.find_element('xpath',search_x_path).click()
-                driver.find_element('xpath',search_text_x_path).send_keys("부업"+j)
-                isHashTag=driver.find_element('xpath',search_result_x_path).text
-                print(isHashTag[0])
-                if isHashTag[0]=='#':
-                    i=i+1
-                    pass
-                else:
-                    driver.find_element('xpath','/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div['+str(i)+']/div/a/div').click()
-                time.sleep(1)
+                driver.find_element('xpath','/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div['+str(i)+']/div/a/div').click()
             except:
                 fail=driver.find_element('xpath','/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div').text
-                print(fail)
                 if fail=='문제가 발생했습니다':
+                    print(fail)
                     print("can't get access")
                     driver.get('https://www.instagram.com')
-                    driver.find_element('xpath','/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]').click()
+                    try:
+                        driver.find_element('xpath','/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]').click()
+                    except:
+                        print('there is no common')
                 else:
                     print("there is no list")
                     break
@@ -87,12 +94,20 @@ for j in list:
             if success=='문제가 발생했습니다. 다시 시도해보세요.':
                 print(success)
                 print("sleep")
-                time.sleep(300)
+                time.sleep(400)
                 driver.refresh()
             else:
                 driver.refresh()
     
-        except Exception as e:
-            print("no setting button")
-            i=i+1
+        except:
+            try:
+                driver.refresh()
+                time.sleep(1)
+                driver.find_element('xpath',setting_x_path).click()
+                driver.find_element('xpath',block_button_x_path).click()
+                driver.find_element('xpath',real_block_x_path).click()
+                success=driver.find_element('xpath','/html/body/div[4]/div[1]/div/div/div/p').text
+                print(success)
+            except:
+                pass
 
